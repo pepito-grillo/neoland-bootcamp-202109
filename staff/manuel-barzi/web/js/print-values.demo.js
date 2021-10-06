b822c8b9af56321a11f3fe490d1bcf114c01ceca
manuelbarzi <manuelbarzi@gmail.com>
2021-10-06 12:13:21 +0200

    update js

---
+++ staff/manuel-barzi/web/js/print-values.demo.js
@@ -0,0 +1,3 @@
+printValues([1, 2, 3, 4, 5])
+
+printValues(['a', 'b', 'c', 'd', 'e'])
--- staff/manuel-barzi/web/js/print-values.js
+++ staff/manuel-barzi/web/js/print-values.js
@@ -1,9 +1,5 @@
-console.log('Hello, World 2!')
-
 // TODO create a function that prints the values of an array in the console
 
 function printValues(values) {
-    console.log(values)
+    //console.log(values)
 }
-
-printValues([1, 2, 3, 4, 5])

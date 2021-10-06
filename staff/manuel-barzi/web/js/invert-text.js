4c4479d784057fc93ed752aebc3aadeb20ee2d01
manuelbarzi <manuelbarzi@gmail.com>
2021-10-06 13:39:34 +0200

    add more js demos

---
+++ staff/manuel-barzi/web/js/invert-text.js
@@ -0,0 +1,9 @@
+function invertText(text) {
+    // NOTE console.log should not be used here, but return
+
+    var result = ''
+
+    // NOTE use for loop to iterate on each character
+
+    return result
+}

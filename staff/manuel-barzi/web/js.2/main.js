767d73dbd2cc21e97625f29e0e68271b9964120e
manuelbarzi <manuelbarzi@gmail.com>
2021-10-06 11:48:50 +0200

    add js exercises

--- staff/manuel-barzi/web/js.2/main.js
+++ staff/manuel-barzi/web/js.2/main.js
@@ -24,32 +24,6 @@ document.write(list)
 
 // TODO build a <table> with headers in <thead> all items from cart and the total in the <tfoot>
 
-function createTableHead(headers) {
-    var heads = ''
-
-    for (var i = 0; i < headers.length; i++) {
-        var header = headers[i]
-
-        heads = heads + createTag('th', header)
-    }
-
-    var head = createTag('thead', createTag('tr', heads))
-
-    return head
-}
-
-function createTableData(content) {
-    return createTag('td', content)
-}
-
-function createTableRow(clazz, content) {
-    return '<tr class="' + clazz + '">' + content + '</tr>'
-}
-
-function createTableFoot(content) {
-    return createTag('tfoot', content)
-}
-
 var bananas = { name: 'Banana', quantity: 4, price: 2 }
 var oranges = { name: 'Orange', quantity: 10, price: 2 }
 var kiwis = { name: 'Kiwi', quantity: 2, price: 6 }
@@ -58,23 +32,21 @@ var melons = { name: 'Melon', quantity: 1, price: 2 }
 var cart = [bananas, oranges, kiwis, melons]
 var total = 0
 
-var table = '<table class="table">' + createTableHead(['name', 'quantity', 'price'])
-
+var table = '<table class="table"><thead><tr><th>name</th><th>quantity</th><th>price</th></tr></thead><tbody>'
 
 for (var i = 0; i < cart.length; i++) { // i++ => i = i + 1
     var item = cart[i]
 
     total = total + item.quantity * item.price
 
-    var datas = createTableData(item.name) + createTableData(item.quantity) +  createTableData(item.price)
-    var row = createTableRow('table__row', datas)
+    var row = '<tr class="table__row"><td>' + item.name + '</td><td>' + item.quantity + '</td><td>' + item.price + '</td></tr>'
 
     table = table + row
 }
 
 table = table + '</tbody>'
 
-var foot = createTableFoot('<tr><th colspan="2">total</th><td>' + total + '</td></tr>')
+var foot = '<tfoot><tr><th colspan="2">total</th><td>' + total + '</td></tr></tfoot>'
 
 table = table + foot + '</table>'
 

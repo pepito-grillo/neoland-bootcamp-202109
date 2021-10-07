d21c7613ac78b01db413fd54b16b4bb5bd9cf92e
manuelbarzi <manuelbarzi@gmail.com>
2021-10-07 11:41:30 +0200

    add more test cases

---
+++ staff/manuel-barzi/web/js/count-numbers.js
@@ -0,0 +1,11 @@
+function countNumbers(values) {
+    var totalPositives = 0
+    var totalNegatives = 0
+    var balance = 0
+
+    // TODO implement me
+
+    var result = [totalPositives, totalNegatives, balance]
+
+    return result
+}

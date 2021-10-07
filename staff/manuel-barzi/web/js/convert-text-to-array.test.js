acbcf98aa736192bfec35cd28765fd11903105b3
manuelbarzi <manuelbarzi@gmail.com>
2021-10-07 11:12:06 +0200

    add more test cases for convert-text-to-array

--- staff/manuel-barzi/web/js/convert-text-to-array.test.js
+++ staff/manuel-barzi/web/js/convert-text-to-array.test.js
@@ -1,5 +1,7 @@
 console.log('TEST convertTextToArray')
 
+// CASE 1
+
 var res = convertTextToArray('hola')
 
 if (res instanceof Array 
@@ -8,6 +10,22 @@ if (res instanceof Array
     && res[1] === 'o' 
     && res[2] === 'l' 
     && res[3] === 'a') {
+    console.log('test ok')
+} else {
+    console.error('test failed')
+}
+
+// CASE 2
+
+var res = convertTextToArray('mundo')
+
+if (res instanceof Array 
+    && res.length === 5
+    && res[0] === 'm' 
+    && res[1] === 'u' 
+    && res[2] === 'n' 
+    && res[3] === 'd'
+    && res[4] === 'o') {
     console.log('test ok')
 } else {
     console.error('test failed')

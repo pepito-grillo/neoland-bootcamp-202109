690e8adb8abd49c9c387e454a649bb8032a7f784
manuelbarzi <manuelbarzi@gmail.com>
2021-10-07 12:14:07 +0200

    add more test cases for invert-text

--- staff/manuel-barzi/web/js/invert-text.test.js
+++ staff/manuel-barzi/web/js/invert-text.test.js
@@ -1,8 +1,19 @@
 console.log('TEST invertText')
 
+// CASE 1
+
 var res = invertText('hola')
 
 if (res === 'aloh')
-    console.log('test ok!')
+    console.log('test ok')
 else
-    console.error('test failed!')
+    console.error('test failed')
+
+// CASE 2
+
+var res = invertText('mundo')
+
+if (res === 'odnum')
+    console.log('test ok')
+else
+    console.error('test failed')

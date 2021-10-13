cab6f7be230580bc589ab441e582d2c200626e31
manuelbarzi <manuelbarzi@gmail.com>
2021-10-13 12:03:49 +0200

    add solution to index-of

--- staff/manuel-barzi/web/js/arrays/index-of.js
+++ staff/manuel-barzi/web/js/arrays/index-of.js
@@ -1,5 +1,16 @@
 // TODO implement the standalone version of Array.prototype.indexOf()
 
-function indexOf(string, value) {
+function indexOf(array, element, index) {
+    // use for loop to iterate on array
+    // use if flow to identify if element is found
+    // return index from element found, otherwise -1
 
+    for (var i = index? index : 0; i < array.length; i++) {
+        var item = array[i]
+
+        if (item === element)
+            return i
+    }
+
+    return -1
 }
--- staff/manuel-barzi/web/js/arrays/index-of.test.js
+++ staff/manuel-barzi/web/js/arrays/index-of.test.js
@@ -2,10 +2,33 @@ describe('TEST indexOf')
 
 // CASE 1
 
-var res = indexOf('hola mundo', 'a')
+var array = ['ant', 'bison', 'camel', 'duck', 'bison']
+var res = indexOf(array, 'bison')
 
 if (typeof res === 'number'
-    && res === 3)
+    && res === 1)
+    success('test ok')
+else
+    fail('test failed')
+
+// CASE 2
+
+var array = ['ant', 'bison', 'camel', 'duck', 'bison']
+var res = indexOf(array, 'bison', 2)
+
+if (typeof res === 'number'
+    && res === 4)
+    success('test ok')
+else
+    fail('test failed')
+
+// CASE 3
+
+var array = ['ant', 'bison', 'camel', 'duck', 'bison']
+var res = indexOf(array, 'giraffe')
+
+if (typeof res === 'number'
+    && res === -1)
     success('test ok')
 else
     fail('test failed')

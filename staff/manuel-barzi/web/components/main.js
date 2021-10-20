700d8b639767911ddcfda766c468c7f9f2e564cb
manuelbarzi <manuelbarzi@gmail.com>
2021-10-20 12:05:28 +0200

    add more js stxff

---
+++ staff/manuel-barzi/web/components/main.js
@@ -0,0 +1,9 @@
+var title = document.createElement('h1')
+title.innerText = 'Hola, Mundo!'
+title.classList.add('title')
+
+console.dir(title)
+
+document.body.append(title)
+
+// TODO mount a login form with js and append it to the body

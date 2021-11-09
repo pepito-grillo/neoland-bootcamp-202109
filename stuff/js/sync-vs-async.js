10e2b019a57479a66c5960c7377468c94bab326b
manuelbarzi <manuelbarzi@gmail.com>
2021-11-09 11:28:20 +0100

    update js sync vs async

--- stuff/js/sync-vs-async.js
+++ stuff/js/sync-vs-async.js
@@ -1,29 +1,29 @@
-console.log('> sync vs async')
+console.log('> sync vs async') // 1
 
-function wait(millis) {
-    var before = Date.now()
+function wait(millis) { // 2
+    var before = Date.now() // 2.1
 
     do {
-        var after = Date.now()
-    } while(after - before < millis)
+        var after = Date.now() // 2.2
+    } while(after - before < millis) // 2.3
 }
 
-console.log('case 1', new Date(), 'start')
+console.log('case 1', new Date(), 'start') // 3
 
-wait(3000)
+wait(3000) // 4
 
-console.log('case 1', new Date(), 'hola mundo')
+console.log('case 1', new Date(), 'hola mundo') // 5
 
-console.log('case 1', new Date(), 'end')
+console.log('case 1', new Date(), 'end') // 6
 
-console.log('case 2', new Date(), 'start')
+console.log('case 2', new Date(), 'start') // 7
 
 setTimeout(function() {
-    console.log('case 2', new Date(), 'hola mundo')
-}, 3000)
+    console.log('case 2', new Date(), 'hola mundo') // 8.1
+}, 3000) // 8
 
-wait(5000)
+wait(5000) // 9
 
-console.log('case 2', new Date(), 'end')
+console.log('case 2', new Date(), 'end') // 10
 
-console.log('etc')
+console.log('etc') // 11

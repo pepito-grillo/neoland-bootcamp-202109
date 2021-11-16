819351e5061f5470b39acbf6a0c56c9c7126d555
manuelbarzi <manuelbarzi@gmail.com>
2021-11-16 10:29:55 +0100

    update agenda

--- staff/manuel-barzi/server-side/agenda/agenda.js
+++ staff/manuel-barzi/server-side/agenda/agenda.js
@@ -1,4 +1,5 @@
-const fs = require('fs')
+const fs = require('fs') // ~ import
+//import fs from 'fs' // WARN it requires package.json
 const { readFile, writeFile } = fs
 
 const { argv: [, , command] } = process
@@ -6,13 +7,12 @@ const { argv: [, , command] } = process
 if (command === 'list') // $ node agenda.js list
     readFile('./contacts.json', 'utf8', (error, json) => {
         if (error) {
-            console.error(error)
+            console.error(error.message)
 
             return
         }
 
         const contacts = JSON.parse(json)
-
 
         console.log('ID NAME PHONE E-MAIL')
 
@@ -35,7 +35,7 @@ else if (command === 'save') // $ node agenda.js save Mario 456456456 mario@mail
         contacts.push({ id: last.id + 1, name, phone, email })
 
         const json2 = JSON.stringify(contacts, null, 4)
-    
+
         writeFile('./contacts.json', json2, error => {
             if (error) {
                 console.error(error.message)
@@ -45,8 +45,74 @@ else if (command === 'save') // $ node agenda.js save Mario 456456456 mario@mail
         })
     })
 else if (command === 'find') // $ node agenda.js find peter
-    console.log('TODO implement me')
-else if (command === 'remove') // $ node agenda.js remove 3
-    console.log('TODO implement me')
-else if (command === 'modify') // $ node agenda.js modify 4 * * peter3@mail.com
-    console.log('TODO implement me')
+    readFile('./contacts.json', 'utf8', (error, json) => {
+        if (error) return console.error(error.message)
+
+        const contacts = JSON.parse(json)
+
+        let { argv: [, , , query] } = process
+
+        query = query.toLowerCase()
+
+        const results = contacts.filter(({ name, phone, email }) => name.toLowerCase().includes(query) || phone.toLowerCase().includes(query) || email.toLowerCase().includes(query))
+
+        console.log('ID NAME PHONE E-MAIL')
+
+        results.forEach(({ id, name, phone, email }) => console.log(id, name, phone, email))
+    })
+else if (command === 'remove') {// $ node agenda.js remove 3
+    const start = Date.now()
+
+    readFile('./contacts.json', 'utf8', (error, json) => {
+        if (error) return console.error(error.message)
+
+        const contacts = JSON.parse(json)
+
+        const { argv: [, , , id] } = process
+
+        const index = contacts.findIndex(contact => contact.id == id)
+
+        if (index < 0) return console.error(`no contact with id ${id} found`)
+
+        contacts.splice(index, 1)
+
+        const json2 = JSON.stringify(contacts, null, 4)
+
+        writeFile('./contacts.json', json2, error => {
+            if (error) return console.error(error.message)
+
+            const end = Date.now()
+
+            console.log(`contact removed (${end - start}ms)`)
+        })
+    })
+} else if (command === 'modify') {// $ node agenda.js modify 4 . . peter3@mail.com
+    const start = Date.now()
+
+    readFile('./contacts.json', 'utf8', (error, json) => {
+        if (error) return console.error(error.message)
+
+        const contacts = JSON.parse(json)
+
+        const { argv: [, , , id, name, phone, email] } = process
+
+        const contact = contacts.find(contact => contact.id == id)
+
+        if (!contact) return console.error(`no contact with id ${id} found`)
+
+        if (name !== '.') contact.name = name
+        // name !== '.' && (contact.name = name)
+        if (phone !== '.') contact.phone = phone
+        if (email !== '.')  contact.email = email
+
+        const json2 = JSON.stringify(contacts, null, 4)
+
+        writeFile('./contacts.json', json2, error => {
+            if (error) return console.error(error.message)
+
+            const end = Date.now()
+
+            console.log(`contact modified (${end - start}ms)`)
+        })
+    })
+}
